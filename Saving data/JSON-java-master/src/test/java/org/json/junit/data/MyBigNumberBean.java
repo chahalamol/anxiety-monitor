a9c786a93e4fcf83eff7b92906d9711fc9bfac2c
SpotCare <80502941+spotcare@users.noreply.github.com>
2021-07-19 11:02:19 -0700

    removed

--- Saving data/JSON-java-master/src/test/java/org/json/junit/data/MyBigNumberBean.java
+++
@@ -1,9 +0,0 @@
-package org.json.junit.data;
-
-/**
- * Used in testing when a Bean containing big numbers is needed
- */
-public interface MyBigNumberBean {
-    public BigInteger getBigInteger();
-    public BigDecimal getBigDecimal();
-}
--- src/main/model/Severity.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-// EFFECTS: sets the enumeration values for Severity
-public enum Severity {
-    MINIMAL, MILD, MODERATE, SEVERE
-}
